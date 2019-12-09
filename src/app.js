e7425f5062a3e9855b379b84ec34881af0554b6b
Shachar Langer <shachar.langer@gmail.com>
2019-12-09 17:25:34 +0200

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import AppRouter, { history } from './routers/AppRouter'
+import configureStore from './store/configureStore'
+import { login, logout } from './actions/auth'
+import 'normalize.css/normalize.css'
+import './styles/styles.scss'
+import 'react-dates/lib/css/_datepicker.css'
+import { firebase } from './firebase/firebase'
+import LoadingPage from './components/LoadingPage'
+
+const store = configureStore()
+
+const jsx = (
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>
+)
+
+let hasRendered = false
+
+// This function was added for cases when we enter the app for the first time.
+// If we're already in the app and we login/logout we don't want to render to app again,
+// we just want to redirect the user
+const renderApp = () => {
+    if (!hasRendered) {
+        ReactDOM.render(jsx, document.getElementById('app'))
+        hasRendered = true
+    }
+}
+
+ReactDOM.render(<LoadingPage />, document.getElementById('app'))
+
+firebase.auth().onAuthStateChanged((user) => {
+    // If the user argument was given, someone logged in.
+    // If the user argument is undefined, someone logged out or there's an initial attempt to check if we're logged in
+    if (user) {
+
+        store.dispatch(login(user.uid))
+        renderApp()
+
+        // We redirect to the dashboard only if we currently on the login page.
+        // If we're on a different page and we refresh for example, we want to stay in the same page
+        if (history.location.pathname === '/') {
+            history.push('/dashboard')
+        }
+    } else {
+        store.dispatch(logout())
+        renderApp()
+        history.push('/')
+    }
+})
